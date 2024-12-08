7bcdbfaeb4b92def89ba030330c05ee386278a15
Greg <fewdarkstar@gmail.com>
2024-12-08 12:38:26 -0600

    added new safe for work tags

--- sfwTags.js
+++ sfwTags.js
@@ -7,6 +7,43 @@ const raiden = document.querySelector("#raiden-shogun");
 const oppai = document.querySelector("#oppai");
 const selfies = document.querySelector("#selfies");
 const uniform = document.querySelector("#uniform");
+const marinKitagawa = document.querySelector("#marin");
+const moriCalliope = document.querySelector("#mori");
+const kamisatoAyaka = document.querySelector("#kamisato");
+
+
+marinKitagawa.addEventListener("click", () => {
+  fetch("https://api.waifu.im/search?included_tags=marin-kitagawa")
+    .then((res) => res.json())
+    .then((res) => {
+      waifuImage.src = res.images[0].url;
+      imageLink.href = res.images[0].source;
+      imageLink.textContent = res.images[0].source;
+    })
+    .catch((err) => console.log(err));
+});
+
+moriCalliope.addEventListener("click", () => {
+  fetch("https://api.waifu.im/search?included_tags=mori-calliope")
+    .then((res) => res.json())
+    .then((res) => {
+      waifuImage.src = res.images[0].url;
+      imageLink.href = res.images[0].source;
+      imageLink.textContent = res.images[0].source;
+    })
+    .catch((err) => console.log(err));
+});
+
+kamisatoAyaka.addEventListener("click", () => {
+  fetch("https://api.waifu.im/search?included_tags=kamisato-ayaka")
+    .then((res) => res.json())
+    .then((res) => {
+      waifuImage.src = res.images[0].url;
+      imageLink.href = res.images[0].source;
+      imageLink.textContent = res.images[0].source;
+    })
+    .catch((err) => console.log(err));
+});
 
 maid.addEventListener("click", () => {
   fetch("https://api.waifu.im/search?included_tags=maid")
